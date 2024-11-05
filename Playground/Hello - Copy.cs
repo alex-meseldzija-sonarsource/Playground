ffc505a8a281e02fba119af6c159b06c627d0afa
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2024-11-05 16:03:09 +0100

    make C# majority number of files:)

---
+++ Playground/Hello - Copy.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections.ObjectModel;
+
+namespace cat
+{
+    public class Hello
+    {
+        private int y; 
+        public static int AddNumber(int a, int b)
+        {
+            int x = 10;
+            return a + b;
+        }
+
+        public static int SubNumber(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+    class CSharp13Enumerables
+    {
+        private ReadOnlySet<int> _setWrapper;
+
+        public int[] SetWrapper
+        {
+            get { return _setWrapper.ToArray(); } // Noncompliant
+        }
+        private OrderedDictionary<int, string> _orderedDict;
+
+        public List<KeyValuePair<int, string>> OrderedDict
+        {
+            get { return _orderedDict.ToList(); } // Noncompliant
+        }
+
+    }
+
+}
