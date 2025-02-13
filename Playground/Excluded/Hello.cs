279425235eb2225ed5022fabc0d7fb987a3c741f
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2025-02-13 10:56:40 +0100

    Testing exclusion on AzDo

---
+++ Playground/Excluded/Hello.cs
@@ -0,0 +1,65 @@
+﻿
+using System.Collections.ObjectModel;
+
+namespace playground.Excluded
+{
+    public class Hello
+    {
+        private int y; 
+
+        public static int AddNumber(int a, int b)
+        {
+            int x = 10;
+            int y = 12;
+            int z = 13;
+            int p = 10;
+            int l = 12;
+            int k = 13;
+            int aa = 10;
+            int bb = 13;
+            int cc = 10;
+            int dd = 10; 
+            return a + b;
+        }
+
+        public static int AddNumberCopy(int a, int b)
+        {
+            int x = 10;
+            int y = 12;
+            int z = 13;
+            int p = 10;
+            int l = 12;
+            int k = 13;
+            int aa = 10;
+            int bb = 13;
+            int cc = 10;
+            int dd = 10;
+            return a + b;
+        }
+
+        public static int SubNumber(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+#if NET9_0_OR_GREATER
+    class CSharp13Enumerables
+    {
+        private ReadOnlySet<int> _setWrapper;
+
+        public int[] SetWrapper
+        {
+            get { return _setWrapper.ToArray(); } // Noncompliant
+        }
+        private OrderedDictionary<int, string> _orderedDict;
+
+        public List<KeyValuePair<int, string>> OrderedDict
+        {
+            get { return _orderedDict.ToList(); } // Noncompliant
+        }
+
+    }
+#endif
+
+}
